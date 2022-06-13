0ca58f7d7b1a5492b9d84982eb23b97c0c106ec1
Larry Kagan <larry@superiocity.com>
2022-06-13 18:45:59 -0400

    Successfully verified and decoded JWT

--- server.js
+++ server.js
@@ -1,27 +1,39 @@
-import express from 'express';
-import jwt from 'jsonwebtoken';
-import dotenv from 'dotenv';
+import express from "express";
+import jwt from "jsonwebtoken";
+import dotenv from "dotenv";
 
 dotenv.config();
 const app = express();
 app.use(express.json());
 
 const posts = [
-    { username: 'Larry', title: 'Post 1' },
-    { username: 'John', title: 'Post 2' },
+    { username: "Larry", title: "Post 1" },
+    { username: "John", title: "Post 2" },
 ];
 
 app.get("/posts", (req, res) => {
     res.json(posts);
 });
 
-app.post("/login", (req, res) => {
+app.post("/login", authenticateToken, (req, res) => {
     // Authenticate user
 
     const username = req.body.username;
     const user = { name: username };
     const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET);
     res.json({ accessToken: accessToken });
-})
+});
 
 app.listen(3000);
+
+function authenticateToken(req, res, next) {
+    const token = req.header("authorization").split(" ")[1];
+
+    try {
+        const decoded = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET)
+        req.user = decoded;
+        next();
+    } catch (e) {
+        res.status(401).json({ message: "Invalid token" });
+    }
+}
